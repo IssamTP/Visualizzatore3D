2a472934b9b6c3de4ed5d58cb5d92a0ef436dae5
IssamTP <53712909+IssamTP@users.noreply.github.com>
2021-10-30 08:14:51 +0200

    Aggiungere i file di progetto.

---
+++ Visualizzatore3D/resource.h
@@ -0,0 +1,32 @@
+//{{NO_DEPENDENCIES}}
+// File di inclusione generato con Microsoft Visual C++.
+// Utilizzato da Visualizzatore3D.rc
+//
+#define IDC_MYICON                      2
+#define IDD_VISUALIZZATORE3D_DIALOG     102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_VISUALIZZATORE3D            107
+#define IDI_SMALL                       108
+#define IDC_VISUALIZZATORE3D            109
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG1                     129
+#define IDD_VISUALIZZATORE3D            129
+#define IDC_LIST1                       1000
+#define IDC_SLIDER1                     1001
+#define IDC_SLIDER2                     1002
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
