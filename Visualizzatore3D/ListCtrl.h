543905b471d1a3808af1c0a10bd89cd14337aa16
Massimiliano Marcianesi <m.marcianesi@issamtp.org>
2021-11-02 08:42:06 +0100

    Avanzamento.

--- Visualizzatore3D/ListCtrl.h
+++ Visualizzatore3D/ListCtrl.h
@@ -5,10 +5,10 @@
 class CListCtrl : public CWindow
 {
 protected:
-	std::vector<std::wstring> m_Etichette;
 	std::vector<LVITEM> m_Elementi;
 public:
 	CListCtrl();
+	~CListCtrl();
 	unsigned int GetNumeroElementi() const;
 	void AggiungiElemento(LPCTSTR descrizione, bool selezionato);
 	void DisplayInfoNotification(LV_DISPINFO* pDisplayInfo);
